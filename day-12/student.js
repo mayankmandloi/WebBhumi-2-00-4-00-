372c1bac0831eb10a22c9cfd50b887cfb94d69de
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-09 16:18:26 +0530

    ....

---
+++ day-12/student.js
@@ -0,0 +1,3 @@
+var bachhe=[
+{Title:"Rakhee",Description:"She is a web designer and a angular developer, she has good hand in graphics design also"},{Title:"Avinash",Description:"He is a Software Engineer and a angular developer, he has good hand in graphics design also"}
+];
