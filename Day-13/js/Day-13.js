cc1451bd3b142c53da69a10ad96e59c8915b93ac
avinashsharma17 <avi.sharma1787@gmail.com>
2017-10-11 15:23:36 +0530

    .

--- Day-13/js/Day-13.js
+++ Day-13/js/Day-13.js
@@ -3,10 +3,11 @@ angular.module("myApp").controller("myController",function ($scope) {
   $scope.person1="Avinash";
   $scope.person2="Rakhee";
   $scope.Books=books;
+  $scope.movies=movies;
 
 });
 angular.module("myApp").config(function ($routeProvider) {
-$routeProvider.when("/",{
+$routeProvider.when("/p1",{
 template:"{{person1}}"
 });
 $routeProvider.when("/p2",{
@@ -17,5 +18,7 @@ $routeProvider.when("/books",{
 templateUrl:"book.html"
 });
 
-
+$routeProvider.when("/movies",{
+templateUrl:"movie.html"
+});
 })
